804491138bdf773172b597ab9f1697ba9b36a887
Nicola Santacroce <nicola.santacroce@protonmail.com>
2025-02-26 14:31:54 +0100

    removed unwanted templates

--- src/napari_vectorizer/__init__.py
+++ src/napari_vectorizer/__init__.py
@@ -3,15 +3,9 @@ try:
 except ImportError:
     __version__ = "unknown"
 from ._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
     threshold_magic_widget,
 )
 
 __all__ = (
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
     "threshold_magic_widget",
 )
--- src/napari_vectorizer/_tests/test_widget.py
+++ src/napari_vectorizer/_tests/test_widget.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 from napari_vectorizer._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
     threshold_magic_widget,
 )
 
@@ -31,36 +28,3 @@ def test_threshold_magic_widget(make_napari_viewer):
     thresholded = my_widget(viewer.layers[0], 0.5)
     assert thresholded.shape == layer.data.shape
     # etc.
-
-
-def test_image_threshold_widget(make_napari_viewer):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-    my_widget = ImageThreshold(viewer)
-
-    # because we saved our widgets as attributes of the container
-    # we can set their values without having to "interact" with the viewer
-    my_widget._image_layer_combo.value = layer
-    my_widget._threshold_slider.value = 0.5
-
-    # this allows us to run our functions directly and ensure
-    # correct results
-    my_widget._threshold_im()
-    assert len(viewer.layers) == 2
-
-
-# capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
-    viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
-
-    # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
-
-    # call our widget method
-    my_widget._on_click()
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"

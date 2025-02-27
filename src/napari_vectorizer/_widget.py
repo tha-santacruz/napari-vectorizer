5198d2a30d8531debd5cbaab18d48b159ce50d59
tha-santacruz <nicola.santacroce@protonmail.com>
2025-02-27 10:51:19 +0100

    added padding to improve contour finding

--- src/napari_vectorizer/_widget.py
+++ src/napari_vectorizer/_widget.py
@@ -29,28 +29,56 @@ References:
 Replace code below according to your needs.
 """
 
+import random
 from typing import TYPE_CHECKING
 
+import numpy as np
 from magicgui import magic_factory
-from skimage.util import img_as_float
-from skimage.measure import find_contours, approximate_polygon
 from napari.layers import Shapes
+from skimage.measure import approximate_polygon, find_contours
+from vispy.color import get_color_names
 
 if TYPE_CHECKING:
     import napari
+
 
 # the magic_factory decorator lets us customize aspects of our widget
 # we specify a widget type for the threshold parameter
 # and use auto_call=True so the function is called whenever
 # the value of a parameter changes
 @magic_factory(
-    contours_tolerance={"widget_type": "FloatSpinBox", "value": 0.5, "min": 0.01, "max": 0.99},
-    polygon_tolerance={"widget_type": "FloatSpinBox", "value": 0.5, "min": 0.00}
+    contours_tolerance={
+        "widget_type": "FloatSpinBox",
+        "value": 0.5,
+        "min": 0.00,
+        "max": 0.99,
+    },
+    polygon_tolerance={
+        "widget_type": "FloatSpinBox",
+        "value": 0.5,
+        "min": 0.00,
+    },
 )
 def label_vectorization_widget(
-    label_layer: "napari.layers.Labels", contours_tolerance: "float", polygon_tolerance: "float"
+    label_layer: "napari.layers.Labels",
+    contours_tolerance: "float",
+    polygon_tolerance: "float",
 ) -> "napari.layers.Shapes":
-    contours = find_contours(label_layer.data, contours_tolerance)
-    polygons = [approximate_polygon(contour, polygon_tolerance) for contour in contours]
-    
-    return Shapes(polygons, shape_type="polygon")
+    # finding contours on padded image (to get outer contours of edge features too)
+    padding_size = 1
+    contours = [
+        np.maximum(contour - 1, 0)
+        for contour in find_contours(
+            np.pad(label_layer.data, padding_size), contours_tolerance
+        )
+    ]
+    # simplifing shapes
+    polygons = [
+        approximate_polygon(contour, polygon_tolerance) for contour in contours
+    ]
+
+    return Shapes(
+        polygons,
+        shape_type="polygon",
+        face_color=random.choice(get_color_names()),
+    )
